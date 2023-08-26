3a43be16edc0dcc3e95d7c7c6e9fa71551fabcb3
Kushagra Mundra <kushsxtxn@gmail.com>
2023-08-26 13:41:45 +0530

    digiClock first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [cTime, setCtime] = useState(new Date().toLocaleTimeString());
+  const [cDate, setcDate] = useState(new Date().toLocaleDateString());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentTime = new Date().toLocaleTimeString();
+      setCtime(currentTime);
+    }, 1000);
+  }, []);
+
+  const date = new Date().toLocaleDateString();
+
+  console.log(date);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ fontSize: '230px', color: '#815854', textAlign: 'center', marginTop: '30vh' }}>{cTime}</h1>
+      <h1 style={{ fontSize: '100px', color: '#815854', textAlign: 'center' }}>{cDate}</h1>
+    </>
   );
 }
 
